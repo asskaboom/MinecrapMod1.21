3aeee9032346110b40c4b5ee943f254acc7c3dbb
TonyBucket057 <afafa057@uottawa.ca>
2024-12-13 21:59:08 -0500

    Initial commit

---
+++ src/main/java/net/tony/minecrap/Minecrap.java
@@ -0,0 +1,17 @@
+package net.tony.minecrap;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Minecrap implements ModInitializer {
+	public static final String MOD_ID = "minecrap";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
