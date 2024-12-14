d6e0daa25af5441ec72c2797c397b9e0358e02a9
TonyBucket057 <afafa057@uottawa.ca>
2024-12-13 23:23:06 -0500

    Initial commit, conveyor belt item added, doesn't work

--- src/main/java/net/tony/minecrap/Minecrap.java
+++ src/main/java/net/tony/minecrap/Minecrap.java
@@ -2,6 +2,7 @@ package net.tony.minecrap;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.tony.minecrap.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,6 +13,6 @@ public class Minecrap implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 	}
 }
